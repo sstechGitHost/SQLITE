07f364c0bd1845c955627663890fd4b7182653e3
Nahid Ahmed Mansuri <nahid.mansuri@gmail.com>
2016-10-17 19:38:07 +0530

    Sample for using SQLite

---
+++ People/People.WinPhone.old/MainPage.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Navigation;
+using Microsoft.Phone.Controls;
+using Microsoft.Phone.Shell;
+
+namespace People.WinPhone
+{
+	public partial class MainPage : global::Xamarin.Forms.Platform.WinPhone.FormsApplicationPage
+	{
+		public MainPage()
+		{
+			InitializeComponent();
+	
+			SupportedOrientations = SupportedPageOrientation.PortraitOrLandscape;
+
+			global::Xamarin.Forms.Forms.Init();
+
+			string dbPath = FileAccessHelper.GetLocalFilePath("people.db3");
+
+			LoadApplication(new People.App(dbPath));
+		}
+	}
+}
